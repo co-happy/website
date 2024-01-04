b45db8b2bc558e907a550737b06817ccd36a688a
Edison <huntack478@gmail.com>
2024-01-04 10:37:51 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,7 @@ import { defaultTheme, defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   lang: 'zh-tw',
   title: '共樂居護所',
+  base: 'website',
   description: '共樂生活股份有限公司附設嘉義縣私立共樂居家',
   theme: defaultTheme({
     navbar: [
